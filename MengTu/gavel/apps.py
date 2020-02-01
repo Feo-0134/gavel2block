34d28af41472626979b9a27fe5bc6c5e19244f18
Feo-0134 <zhujuncheng1021@gmail.com>
2020-02-01 13:10:42 +0800

    start drf project

---
+++ MengTu/gavel/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GavelConfig(AppConfig):
+    name = 'gavel'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/junchengzhu/workbranch/gavel2block/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
